217e88c20e060d79844bba05de9ca1937543707d
cielaton <vuwin24680@gmail.com>
2024-08-17 21:24:35 +0700

    [Database & Handler] Implemented the POST method for development tools.

--- server/database/insert/insert_development_tools.go
+++ server/database/insert/insert_development_tools.go
@@ -1,7 +1,7 @@
 package insert
 
 import (
-	"fmt"
+	"context"
 	"github.com/jackc/pgx/v5"
 	"server/models"
 	"strconv"
@@ -10,13 +10,17 @@ import (
 
 func DevelopmentToolsInsert(database *pgx.Conn, developmentTools models.DevelopmentTools) error {
 
-	// This is the slice without integer values span and order value, which will be added later
+	// This is the slice of the contents inside the VALUES() command, will be separated by the comma
 	inputDevelopmentToolsDataSlice := []string{"'" + developmentTools.Field + "'", "'" + developmentTools.Description + "'", "'" + developmentTools.Style.TextColor + "'", strconv.Itoa(developmentTools.Style.Span), strconv.Itoa(developmentTools.Order)}
 
 	// Inserts into two tables using WITH
 	queryCommand := "WITH favourite_tools_insertion " +
-		"AS (INSERT INTO favourite_tools (field, descriptions, text_color, span, \"order\") VALUES (" + strings.Join(inputDevelopmentToolsDataSlice, ",") + ") RETURNING field) " + "INSERT INTO favourite_tools_icons (field, path, icon_names)" + "VALUES ((SELECT field FROM favourite_tools_insertion), '" + developmentTools.Icons.Path + "', ARRAY['" + strings.Join(developmentTools.Icons.Name, "','") + "']"
+		"AS (INSERT INTO favourite_tools (field, descriptions, text_color, span, \"order\") VALUES (" + strings.Join(inputDevelopmentToolsDataSlice, ",") + ") RETURNING field) " + "INSERT INTO favourite_tools_icons (field, path, icon_names)" + "VALUES ((SELECT field FROM favourite_tools_insertion), '" + developmentTools.Icons.Path + "', ARRAY['" + strings.Join(developmentTools.Icons.Name, "','") + "'])"
 
-	fmt.Println(queryCommand)
+	_, err := database.Exec(context.Background(), queryCommand)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- server/handlers/development_tools.go
+++ server/handlers/development_tools.go
@@ -5,10 +5,30 @@ import (
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"server/database/get"
+	"server/database/insert"
+	"server/models"
 )
 
 func GetDevelopmentTools(c echo.Context) error {
 	database := c.Get("database").(*pgx.Conn)
 	queryResult := get.DevelopmentToolsQuery(database)
 	return c.JSON(http.StatusOK, queryResult)
+}
+
+func InsertDevelopmentTools(c echo.Context) error {
+	database := c.Get("database").(*pgx.Conn)
+	developmentTools := new(models.DevelopmentTools)
+
+	err := c.Bind(developmentTools)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
+	}
+
+	err = insert.DevelopmentToolsInsert(database, *developmentTools)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Internal server error")
+	}
+
+	return c.JSON(http.StatusCreated, developmentTools)
+
 }
